33d74a26eff65ed784371d141f6ffd02bfc5f6e1
ljh00112233 <l268728@naver.com>
2025-05-13 17:18:42 +0900

    0513_Header.js
    헤더

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Header from './components/layout.js/Header';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Header />} />
         <Route path="/login" element={<Login />} /> {/* ✅ 대문자! */}
       </Routes>
     </Router>
---
+++ client/src/components/layout.js/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+/**
+ * Header Component (Bootstrap)
+ * - 고정 상단 네비게이션 바
+ * - Home, Portfolio, Login 링크 제공
+ */
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container">
+        {/* 로고 / 사이트 이름 */}
+        <Link className="navbar-brand" to="/">
+          LJHworks
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/portfolio"
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/login"
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
